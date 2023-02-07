1ca3dcedb82488678fa2c42aadd1e2cf225e0be6
Architektur-Design] <53041323+MudBloodArchitekt@users.noreply.github.com>
2023-02-07 14:22:42 +0100

    update

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -7,12 +7,21 @@ import {
   NotAuthorizedError,
 } from '@alimtickets/common';
 import { Ticket } from '../models/ticket';
+import { TicketUpdatedPublisher } from '../events/publishers/ticket-updated-publisher';
+import { natsWrapper } from '../nats-wrapper';
 
 const router = express.Router();
 
 router.put(
   '/api/tickets/:id',
   requireAuth,
+  [
+    body('title').not().isEmpty().withMessage('Title is required'),
+    body('price')
+      .isFloat({ gt: 0 })
+      .withMessage('Price must be provided and must be greater than 0'),
+  ],
+  validateRequest,
   async (req: Request, res: Response) => {
     const ticket = await Ticket.findById(req.params.id);
 
@@ -29,6 +38,12 @@ router.put(
       price: req.body.price,
     });
     await ticket.save();
+    new TicketUpdatedPublisher(natsWrapper.client).publish({
+      id: ticket.id,
+      title: ticket.title,
+      price: ticket.price,
+      userId: ticket.userId,
+    });
 
     res.send(ticket);
   }
