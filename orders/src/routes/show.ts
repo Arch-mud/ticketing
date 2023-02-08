72a4861a1c629babd5ba394809cea617b203e3a7
Architektur-Design] <53041323+MudBloodArchitekt@users.noreply.github.com>
2023-02-08 11:59:11 +0100

    update order service

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,18 +1,18 @@
-import express, { Request, Response } from 'express';
+import express, { Request, Response } from "express";
 import {
   requireAuth,
   NotFoundError,
   NotAuthorizedError,
-} from '@alimtickets/common';
-import { Order } from '../models/order';
+} from "@alimtickets/common";
+import { Order } from "../models/order";
 
 const router = express.Router();
 
 router.get(
-  '/api/orders/:orderId',
+  "/api/orders/:orderId",
   requireAuth,
   async (req: Request, res: Response) => {
-    const order = await Order.findById(req.params.orderId).populate('ticket');
+    const order = await Order.findById(req.params.orderId).populate("ticket");
 
     if (!order) {
       throw new NotFoundError();
@@ -24,6 +24,5 @@ router.get(
     res.send(order);
   }
 );
-
 
 export { router as showOrderRouter };
