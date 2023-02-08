fe8926ad40a16260d5fecbc985c809f6015ee897
Architektur-Design] <53041323+MudBloodArchitekt@users.noreply.github.com>
2023-02-08 11:20:50 +0100

    order module update

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,9 +3,11 @@ import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@alimtickets/common';
+
+import { deleteOrderRouter } from './routes/delete';
+import { indexOrderRouter } from './routes/index';
 import { newOrderRouter } from './routes/new';
 import { showOrderRouter } from './routes/show';
-import { indexOrderRouter } from './routes/index';
 
 const app = express();
 app.set('trust proxy', true);
@@ -18,9 +20,10 @@ app.use(
 );
 app.use(currentUser);
 
+app.use(deleteOrderRouter);
+app.use(indexOrderRouter);
 app.use(newOrderRouter);
 app.use(showOrderRouter);
-app.use(indexOrderRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();
