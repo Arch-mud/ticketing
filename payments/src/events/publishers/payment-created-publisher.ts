c97562a6d59c85636e6efecca44f3c1693289683
Architektur-Design] <53041323+MudBloodArchitekt@users.noreply.github.com>
2023-02-10 16:27:10 +0100

    refactor

--- payments/src/events/publishers/payment-created-publisher.ts
+++ payments/src/events/publishers/payment-created-publisher.ts
@@ -1,5 +1,5 @@
 import { Subjects, Publisher, PaymentCreatedEvent } from '@alimtickets/common';
 
 export class PaymentCreatedPublisher extends Publisher<PaymentCreatedEvent> {
-  subject: Subjects.PaymentCreated = Subjects.PaymentCreated;
+  readonly subject = Subjects.PaymentCreated;
 }
